e4b1464a1525e5d13ed18c0e1f92999aebe5279d
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-01 10:40:22 +0000

    Add files

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ console.error("Capricorn");
 
 text = "Experimenting with CSS properties"
 console.log('%C' + text, 'font-family:fantasy', 'color:orange','background-color:black', 'font-style:bold', 'padding:10px' )
+
+console.log("Hello World!");
+
+let text = `this is a message`;
+
+console.log(text);
+
+console.info(text);
+
+console.warn(text);
+
+console.error(text);
+
+console.log(`%c` + text, `color:black; background-color:white; font-size: 20px; padding: 10px;`);
+
+console.log(`%c` + text + ` ` + `%c` + text, `color: red;`, `color:blue;`);
