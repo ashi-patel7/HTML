79f2d788d463d1a5ccae41d794c659afc0932698
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-01 10:31:27 +0000

    Added files

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ console.info("Patel");
 console.warn("London");
 console.error("Capricorn");
 
-console.log()
+text = "Experimenting with CSS properties"
+console.log('%C' + text, 'font-family:fantasy', 'color:orange','background-color:black', 'font-style:bold', 'padding:10px' )
