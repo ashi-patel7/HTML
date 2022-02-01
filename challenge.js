830f6dba10845a26dc752d35f3e214337c93e060
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-01 16:26:54 +0000

    Tuesday challenge

--- challenge.js
+++ challenge.js
@@ -18,8 +18,17 @@ for(let i = 1; i<=100; i++){ //iterate to 100
     }
 }
 
-for(let i=1; i<=100; i++) console.log(i%15==0?) //continue 
-
 //Write a program that will print the song "99 bottles of beer on the wall", based on the lyrics
 // x bottles of beer on the wall, x bottles of beer. take one down and pass it around x-1 bottles on the wall
 
+let bottles = 99;
+while (bottles){
+    switch(bottles){
+      case 1:
+        console.log(`1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, ${bottles--} bottles of beer on the wall.`);
+        console.log(`No more bottles of beer on the wall, No more bottle of beer. Go to the store and buy some more, 99 bottles of beer on the wall.`);
+        break;
+      default:
+        console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, ${bottles--} bottles of beer on the wall.`);
+    }
+  }
