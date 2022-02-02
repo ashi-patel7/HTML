26d150d2f73bf6182abb00a39b5d626cf61ea139
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-02 09:48:22 +0000

    Add files

--- wednesday.js
+++ wednesday.js
@@ -5,3 +5,8 @@ let darthVader = {
 };
 
 console.log(darthVader);
+
+console.log(`Darth Vader's allegiance is to the ${darthVader.allegiance}`);
+console.log(`Darth Vader's weapon of choice is a ${darthVader.weapon}`);
+console.log(`Darth Vader is a sith? ${darthVader.sith}`);
+console.log(`Darth Vader is a Jedi? ${darthVader.sith ? "false" : "true"}`);
