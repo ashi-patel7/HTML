b963e77b385075b2a4ed6d7fee62c162494f8e62
Ayshwaria Patel <ashi_patel@icloud.com>
2022-02-02 09:56:57 +0000

    Wednesday morning task

--- wednesday.js
+++ wednesday.js
@@ -10,3 +10,17 @@ console.log(`Darth Vader's allegiance is to the ${darthVader.allegiance}`);
 console.log(`Darth Vader's weapon of choice is a ${darthVader.weapon}`);
 console.log(`Darth Vader is a sith? ${darthVader.sith}`);
 console.log(`Darth Vader is a Jedi? ${darthVader.sith ? "false" : "true"}`);
+
+let MyArray = Array('hello', 'everyone');
+
+console.log(MyArray.length);
+
+console.log(MyArray.push('today', 'is', 'wednesday'));
+
+console.log(MyArray.length);
+
+MyArray.shift();
+
+for(let word of MyArray){
+    console.log(word);
+}
